5f74dd80dc6c884f69631c32e9417d79abd99b4c
Rodrich272 <145025307+Rodrich272@users.noreply.github.com>
2023-10-19 07:27:26 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,14 +1,8 @@
 var nav=document.querySelector('nav')
 window.addEventListener('scroll', function(){
-    if( window.pageXOffset > 20){
+    if(this.window.pageXOffset > 20){
         nav.classList.add('bg-dark', 'shadow');
     }else{
         nav.classList.remove('big-dark', 'shadow')
     }
 })
-
-AOS.init({
-    easing: 'ease-out-back',
-    duration: 1000
-});
-
