5bbd369dd70b31116a126782f64a68a1707732b1
Rodrich272 <145025307+Rodrich272@users.noreply.github.com>
2023-12-07 07:58:22 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-var nav=document.querySelector('nav')
-window.addEventListener('scroll', function(){
-    if(this.window.pageXOffset > 20){
-        nav.classList.add('bg-dark', 'shadow');
-    }else{
-        nav.classList.remove('big-dark', 'shadow')
-    }
-})
+var nav = document.querySelector("nav");
+window.addEventListener("scroll", function () {
+  if (this.window.pageXOffset > 20) {
+    nav.classList.add("bg-dark", "shadow");
+  } else {
+    nav.classList.remove("big-dark", "shadow");
+  }
+});
--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-var nav=document.querySelector('nav')
-window.addEventListener('scroll', function(){
-    if( window.pageXOffset > 20){
-        nav.classList.add('bg-dark', 'shadow');
-    }else{
-        nav.classList.remove('big-dark', 'shadow')
-    }
-})
-
-AOS.init({
-    easing: 'ease-out-back',
-    duration: 1000
+var nav = document.querySelector("nav");
+window.addEventListener("scroll", function () {
+  if (window.pageXOffset > 20) {
+    nav.classList.add("bg-dark", "shadow");
+  } else {
+    nav.classList.remove("big-dark", "shadow");
+  }
 });
 
+AOS.init({
+  easing: "ease-out-back",
+  duration: 1000,
+});
+const myModal = document.getElementById("myModal");
+const myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", () => {
+  myInput.focus();
+});
